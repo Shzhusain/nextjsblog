bb35a1500ec7a0d08a72a004ae38dbb4b614c2e2
Shzhusain <Shzhussain5@gmail.com>
2024-04-25 16:33:16 +0500

    Why Choose us Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
+import WhyChooseUS from "@/components/WhyChooseUS";
+
 
 export default function Home() {
   return (
-  <main>
+  <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
+    <FeaturedCourses/>
+    <WhyChooseUS/>
+   
+  
 
   </main>
   ) 
